cc00bf22b0bb899bbbf77d9ba85c3ac43587596b
Vins <sharmavins23@gmail.com>
2020-11-29 04:18:50 -0500

    added selection screen limited functionality

--- SCGD-Engine-Source.ino
+++ SCGD-Engine-Source.ino
@@ -12,8 +12,16 @@
 #include "src/SCGDEngineGamesList.h" // Contains game prototypes
 
 // Global variables
-#define NUM_OF_GAMES = 1
+#define MENU_NUM_OF_GAMES 1  // The current number of games
+#define MENU_ICON_SIZE 60    // Size (px) of an icon, height and width
 int hasEnteredSelection = 0; // Tracks whether player has stopped viewing menu
+int hasChosenGame = 0;       // Tracks whether player has selected game
+typedef struct {
+    int xPos; // Pixel X position
+    int yPos; // Pixel Y position
+    int game; // Game number (from 0 through 8)
+} menu_cursor_t;
+menu_cursor_t menuCursor; // Tracks current selected game (supports max 9)
 
 // ===== Initialization ========================================================
 
@@ -36,11 +44,13 @@ void setup() {
 // ===== Menu looping ==========================================================
 
 void loop() {
-    // First, handle the
+    // First, handle the popup
     if (!hasEnteredSelection) {
         handlePopupClose();
         return; // Continue to next iteration of loop()
     }
+
+    handleMenuSelect();
 }
 
 // ===== Controller ============================================================
@@ -62,6 +72,39 @@ void handlePopupClose() {
             ;
 
         menuDrawSelectionScreen();
+        hasEnteredSelection = 1; // Track that we've seen the label
+    }
+}
+
+void handleMenuSelect() {
+    while (true) {
+        // TODO: Implement selection box moving around
+        // ...
+        if (digitalRead(B_BUTTON)) {
+            while (digitalRead(B_BUTTON))
+                ;
+
+            if (hasChosenGame) {
+                hasChosenGame = 0;
+                menuDrawSelectionScreenAgain();
+            }
+        }
+        if (digitalRead(A_BUTTON)) {
+            while (digitalRead(A_BUTTON))
+                ;
+
+            if (!hasChosenGame) {
+                drawMessageBoxTwoLines(tft, TFT_WHITE,
+                                       // Line 1
+                                       "  BATTLESHIP  ", TFT_WHITE,
+                                       // Line 2
+                                       "   (A) Play   ", TFT_YELLOW);
+                hasChosenGame = 1;
+            } else {
+                // TODO: Play battleship
+                tft.fillScreen(TFT_BLACK);
+            }
+        }
     }
 }
 
@@ -89,7 +132,45 @@ void menuDrawSelectionScreen() {
     drawMarginBoundingBox(tft, TFT_WHITE);
 
     // Draw individual games
-    battleshipDrawGameIcon(tft, STD_WORKING_MARGIN * 2, STD_WORKING_MARGIN * 2);
+    battleshipDrawGameIcon(tft, 20, 20);
+
+    // Draw cursor
+    menuCursor.game = 0; // Start at top left, on battleship
+    menuDrawSelectionBox(0);
+}
+
+// Same as before, but without re-initializing cursor
+void menuDrawSelectionScreenAgain() {
+    tft.fillScreen(TFT_BLACK);
+    drawMarginBoundingBox(tft, TFT_WHITE);
+
+    // Draw individual games
+    battleshipDrawGameIcon(tft, 20, 20);
+
+    // Draw cursor
+    menuDrawSelectionBox(menuCursor.game);
+}
+
+menu_cursor_t menuRecalcCursor(int game) {
+    // Create a new cursor structure
+    menu_cursor_t cursor;
+    cursor.game = game;
+    // Calculate X position - Adding 2 margins and icon sizing up to 3x
+    cursor.xPos = 20 + (70 * (game % 3));
+    // Calculate Y position - 2 margins and icon sizing up to 3x
+    cursor.yPos = 20 + (70 * (game / 3));
+
+    return cursor;
+}
+
+void menuDrawSelectionBox(int newGame) {
+    menuCursor = menuRecalcCursor(menuCursor.game); // Recalculate cursor values
+    // Replace selection box at cursor position with white box
+    tft.drawRect(menuCursor.xPos, menuCursor.yPos, 60, 60, TFT_WHITE);
+
+    menuCursor = menuRecalcCursor(newGame); // Create new cursor
+    // Draw a new selection box at the new position
+    tft.drawRect(menuCursor.xPos, menuCursor.yPos, 60, 60, TFT_YELLOW);
 }
 
 // ===== Webserver =============================================================
